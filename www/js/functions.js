1c6c8b93f3b07a0f561eb96a0932886d97a7931a
dagmara <dagmara.223@gmail.com>
2015-04-01 17:22:02 +0200

    poprawki & wersja en

--- www/js/functions.js
+++ www/js/functions.js
@@ -2,6 +2,22 @@ $(function() {
 	$('#menu').mmenu({
             offCanvas: {
                position  : "right"
-            }
-         });
+            },
+            //searchfield: true
+         }, {
+         // configuration
+         classNames: {
+            fixedElements: {
+               fixed: "fixed"
+         	}
+         } 
+   }); 
+   // mmenu przeslania footer z linkami, obejscie
+	$('#mm-0').css("z-index", "-1");
+
+	// menu wersja desktop
+	$('.menu').dropit({ 
+		action: 'mouseenter' 
+	});
+	
 }); 
